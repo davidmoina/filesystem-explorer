8385eb3486739adf4a28fadfad5539686f3af7ec
davidmoina <luisdavidmoina@gmail.com>
2023-01-10 12:37:08 +0100

    finished first html structure

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <script defer src="https://kit.fontawesome.com/8bbf7b9ae4.js" crossorigin="anonymous"></script>
+    <title>Drive</title>
+</head>
+<body>
+    <header>
+        <div>
+            <h1>Drive</h1>
+        </div>
+        <div>
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <input type="text" name="" id="" placeholder="Please enter some here">
+        </div>
+    </header>
+
+    <main>
+        <aside class="aside-menu">
+            <button><i class="fa-solid fa-folder-plus"></i> New folder</button>
+            <button><i class="fa-solid fa-file-arrow-up"></i> Upload file</button>
+        </aside>
+
+        <section>
+            <div class="menu-section">
+                <div>
+                    <i class="fa-solid fa-house"></i>
+                    <i class="fa-solid fa-left-long"></i>
+                </div>
+                <div>
+                    <i class="fa-solid fa-trash-can"></i>
+                    <i class="fa-solid fa-copy"></i>
+                    <i class="fa-solid fa-scissors"></i>
+                    <i class="fa-solid fa-paste"></i>
+                    <i class="fa-solid fa-pen-to-square"></i>
+                </div>
+            </div>
+            <div class="route-section">
+                <p>Home/</p>
+            </div>
+            <div class="info-header">
+                <p>Name</p>
+                <p>Last Modified</p>
+                <p>Creation date</p>
+                <p>Size</p>
+            </div>
+            <div class="files-section">
+                <div>
+                    <p><i class="fa-solid fa-file"></i> text1.txt</p>
+                    <p>10/01/2023</p>
+                    <p>10/01/2023</p>
+                    <p>300Kb</p>
+                </div>
+            </div>
+        </section>
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+</html>
