3f67d8dbffaf3548c1b2c047b95e9a561c5185de
davidmoina <luisdavidmoina@gmail.com>
2023-01-11 09:23:08 +0100

    fixed bug

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,15 +4,15 @@ const modalShowImg = document.querySelector(".modal-show-img");
 btnUploadFile.addEventListener("change", uploadFile);
 
 function uploadFile() {
-  let formData = new FormData();
-  formData.append("fileData", btnUploadFile.files[0]);
+    let formData = new FormData();
+    formData.append("fileData", btnUploadFile.files[0]);
 
-  fetch("modules/uploadFile.php", {
-    method: "POST",
-    body: formData,
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    });
+    fetch("modules/uploadFile.php", {
+        method: "POST",
+        body: formData,
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+        });
 }
