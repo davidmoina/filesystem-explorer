0914cd543b4b404cec37a2a71cb6f184f4cb0105
davidmoina <luisdavidmoina@gmail.com>
2023-01-11 12:40:12 +0100

    added create new folder feature

--- modules/create-folder.php
+++ modules/create-folder.php
@@ -1,10 +1,12 @@
 <?php
 
     $newFolder = "new_folder";
+    $route = dirname(__DIR__) . "/root";
+    $directory = str_replace('\\', '/', $route) . "/" . $newFolder;
 
-    if(!is_dir($newFolder)) {
-        mkdir($newFolder);
+    if(!is_dir($directory)) {
+        mkdir($directory);
         echo "Folder created successfully";
     } else {
-        echo "The folder $newFolder already exist!";
+        echo "The folder $directory already exist!";
     }
