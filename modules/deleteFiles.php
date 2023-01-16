aef19af704d809a9cfd4704d4c66c806d18c6827
Isaura Martí <imarti.teixido@gmail.com>
2023-01-16 17:22:00 +0100

    delete folders with contents

--- modules/deleteFiles.php
+++ modules/deleteFiles.php
@@ -6,9 +6,28 @@ $path = $_GET["path"];
 $url_insert = dirname(__DIR__) . $path;
 $url_target = str_replace('\\', '/', $url_insert) . '/' . $name;
 
+function deleteFolder($dirname)
+{
+    if (is_dir($dirname))
+        $dir_handle = opendir($dirname);
+    if (!$dir_handle)
+        return false;
+
+    while ($file = readdir($dir_handle)) {
+        if ($file != "." && $file != "..") {
+            if (!is_dir($dirname . "/" . $file))
+                unlink($dirname . "/" . $file);
+            else deleteFolder($dirname . '/' . $file);
+        }
+    }
+    closedir($dir_handle);
+    rmdir($dirname);
+    return true;
+}
+
 $type =  filetype($url_target);
 if (is_dir($url_target)) {
-    rmdir($url_target);
+    deleteFolder($url_target);
 } else {
     unlink($url_target);
 }
@@ -17,5 +36,3 @@ if (file_exists($url_target) === false) {
     if ($type === "dir") $type = "folder";
     echo json_encode("The $type $name has been deleted");
 }
-
-?>
